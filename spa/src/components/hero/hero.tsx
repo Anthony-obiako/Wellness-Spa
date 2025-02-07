b8e579ee3315bae84906117a02620a8b672d3910
Anthony Obiako <anthonyobiakoc@gmail.com>
2025-02-07 08:59:25 +0100

    fixed issue

--- spa/src/components/hero/hero.tsx
+++ spa/src/components/hero/hero.tsx
@@ -6,18 +6,18 @@ import Button from "@/components/button/button";
 export default function Hero() {;
 
   return (
-    <section className="relative h-screen w-full flex justify-center bg-cover bg-center bg-[url('/images/home-4.jpg')] before:absolute before:inset-0 before:bg-black/30 before:z-0 pt-48">
+    <section className="relative h-screen w-full flex items-center justify-center bg-cover bg-center bg-[url('/images/home-4.jpg')] before:absolute before:inset-0 before:bg-black/30 before:z-0 pt-48">
+      <div className="absolute inset-x-0 bottom-0 h-1/6 bg-gradient-to-t from-black/90 from-40% to-transparent to-90%" />
       {/* Animated Content */}
       <motion.div
         initial={{ opacity: 0, y: 100 }}
         whileInView={{ opacity: 1, y: 0 }}
         viewport={{ once: true, amount: 0.5 }}
         transition={{ duration: 1, ease: "easeOut" }}
-        className="text-center text-white"
+        className="text-center text-white justify-items-center items-center"
       >
-        <div className="absolute inset-x-0 bottom-0 h-1/6 bg-gradient-to-t from-black/90 to-transparent" />
-        <div className="z-10 relative px-12">
-          <h1 className="mb-10 lg:text-7xl/tight md:text-5xl/tight text-2xl/normal text-white font-activa max-w-[1000px] ">
+        <div className="z-10 relative px-12 justify-items-center items-center -mt-32">
+          <h1 className="md:mb-10 mb-3 lg:text-7xl/tight md:text-5xl/tight text-3xl/normal text-white font-activa max-w-[1000px] ">
             AB WELLNESS
             <span className="block">
               A SPACE FOR RELAXATION OF BODY AND SOUL
@@ -27,7 +27,7 @@ export default function Hero() {;
             Discover a new source of energy and vitality.
           </p>
           <Button
-            className="mt-6 bg-black text-amber-50 px-5 py-4 rounded-full"
+            className="md:mt-6 mt-5 bg-black text-amber-50 px-10 py-4 rounded-full"
             modalContent
           >
             Book a Visit
